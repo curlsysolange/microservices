555bbcad7ea7cb38c8d381c6552c935331370319
Curlsy Solange <curlsysolange>
2025-01-13 22:38:08 -0500

    Terraform updated

--- EKTS-TF/backend.tf
+++ EKTS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "curlsybucket" # Replace with your actual S3 bucket name
+    bucket = "curlsybuckets" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-2"
   }
